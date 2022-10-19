679ac4be328357ee153e7adacca6e58193755b2e
chicodevbr <chicodevbr@gmail.com>
2022-10-18 21:27:56 -0300

    initial project

--- GridCalendar/ContentView.swift
+++ GridCalendar/ContentView.swift
@@ -19,6 +19,43 @@ struct ContentView: View {
     }
 }
 
+
+struct Month {
+    let name: String
+    let numberOfDays: Int
+    var days: [Day]
+    
+    init(name: String, numberOfDays: Int) {
+        self.name = name
+        self.numberOfDays = numberOfDays
+        self.days = []
+        
+        for n in 1...numberOfDays {
+            self.days.append(Day(value: n))
+        }
+    }
+}
+
+struct Day: Identifiable {
+    let id = UUID()
+    let value : Int
+}
+
+let year = [
+       Month(name: "January", numberOfDays: 31),
+       Month(name: "February", numberOfDays: 28),
+       Month(name: "March", numberOfDays: 31),
+       Month(name: "April", numberOfDays: 30),
+       Month(name: "May", numberOfDays: 31),
+       Month(name: "June", numberOfDays: 30),
+       Month(name: "July", numberOfDays: 31),
+       Month(name: "August", numberOfDays: 31),
+       Month(name: "September", numberOfDays: 30),
+       Month(name: "October", numberOfDays: 31),
+       Month(name: "November", numberOfDays: 30),
+       Month(name: "December", numberOfDays: 31),
+   ]
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
